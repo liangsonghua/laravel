b593d1abb0eec152fea86d68e22020687a43cea5
liangsonghua <1411008034@qq.com>
2017-06-05 13:01:23 +0800

    GuzzleHttp

--- app/Util/parserBody.php
+++ app/Util/parserBody.php
@@ -5,11 +5,11 @@ use App\Repositories\ChannelRepository;
 use App\Events\Broadcast;
 class parserBody {
 
-	public function __construct($type,$keyword,$body)
+	public function __construct($type,$channel_id,$body)
 	{
 		switch ($type) {
 			case 'douban':
-				$this->parserDouban($keyword,$body);
+				$this->parserDouban($channel_id,$body);
 				break;
 			default:
 				# code...
@@ -17,25 +17,25 @@ class parserBody {
 		}
 	}
 
-	public function parserDouban($keyword, $body)
+	public function parserDouban($channel_id, $body)
 	{
-		$channel = ChannelRepository::where('keyword','like',$keyword)->firstOrFail();
+
 		$pattern  ='/<tr class=\"pl\">(.*?)<\/tr>/s';
 		$p  ='/<td class=\"td\-subject\"><a class=\"\" href=\"(.*?)\" title=\"(.*?)\">(.*?)<\/a>(.*?)<td class=\"td\-time\" title=\"(.*?)\" nowrap=\"nowrap\"><span class=\"\">/s';
 		preg_match_all($pattern, $body,$matches);
 		foreach ($matches[0] as $key => $value) {	
 			preg_match($p,$value,$res);
 			if(date('Ymd',strtotime($res[5]))>=date('Ymd',time())) {
-				$update = array(
+				$data = array(
 						'url'=>$res[1],
 						'title'=>$res[2],
 						'time'=>$res[5],
-						'channel_id'=>$channel->id
+						'channel_id'=>$channel_id,
 					);
 				$daoDouban = new DoubanRepository();
-				$daoDouban->fill($update);
+				$daoDouban->fill($data);
 				$daoDouban->save();
-				event(new Broadcast($daoDouban));	
+				// event(new Broadcast($daoDouban));	
 				
 			}
 		}
